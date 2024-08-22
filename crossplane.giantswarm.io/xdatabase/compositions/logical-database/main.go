f18e9c2d1fddc07c5fdda39fe41514e2a81f428e
Martin Proffitt <mproffitt@choclab.net>
2024-08-22 14:30:11 +0200

    Add logical database composition

---
+++ crossplane.giantswarm.io/xdatabase/compositions/logical-database/main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"strings"
+
+	"crossbuilder/v1alpha1"
+
+	xkcl "github.com/crossplane-contrib/function-kcl/input/v1beta1"
+
+	xapiextv1 "github.com/crossplane/crossplane/apis/apiextensions/v1"
+	"github.com/mproffitt/crossbuilder/pkg/generate/composition/build"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+type builder struct{}
+
+var Builder = builder{}
+
+func (b *builder) GetCompositeTypeRef() build.ObjectKindReference {
+	return build.ObjectKindReference{
+		GroupVersionKind: v1alpha1.RdsLogicalDatabaseGroupVersionKind,
+		Object:           &v1alpha1.RdsLogicalDatabase{},
+	}
+}
+
+func (b *builder) Build(c build.CompositionSkeleton) {
+	c.WithName("logical-database").
+		WithMode(xapiextv1.CompositionModePipeline).
+		WithLabels(map[string]string{
+			"provider":  "aws",
+			"component": "database",
+			"type":      "provisioning",
+		})
+
+	var (
+		kclCommon      string
+		kclSqlTemplate string
+		err            error
+	)
+
+	kclCommon, err = build.LoadTemplate("compositions/rds-provisioning/templates/common.k")
+	if err != nil {
+		panic(err)
+	}
+
+	kclSqlTemplate, err = build.LoadTemplate("compositions/rds-provisioning/templates/provision-sql.k")
+	if err != nil {
+		panic(err)
+	}
+
+	kclFooter := "items = _items"
+
+	c.NewPipelineStep("function-provision-sql").
+		WithFunctionRef(xapiextv1.FunctionReference{
+			Name: "function-kcl",
+		}).
+		WithInput(build.ObjectKindReference{
+			Object: &xkcl.KCLInput{
+				TypeMeta: metav1.TypeMeta{
+					APIVersion: "krm.kcl.dev/v1alpha1",
+					Kind:       "KCLInput",
+				},
+				Spec: xkcl.RunSpec{
+					Source: strings.Join([]string{kclCommon, kclSqlTemplate, kclFooter}, "\n\n"),
+				},
+			},
+		})
+
+	c.NewPipelineStep("function-auto-ready").
+		WithFunctionRef(xapiextv1.FunctionReference{
+			Name: "function-auto-ready",
+		})
+}
---
+++ crossplane.giantswarm.io/xdatabase/v1alpha1/logicaldatabase_types.go
@@ -0,0 +1,99 @@
+package v1alpha1
+
+import (
+	xpv1 "github.com/crossplane/crossplane-runtime/apis/common/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+)
+
+// +kubebuilder:object:root=true
+// +kubebuilder:storageversion
+// +genclient
+// +genclient:nonNamespaced
+//
+// +kubebuilder:resource:scope=Cluster,categories=crossplane
+// +kubebuilder:subresource:status
+// +kubebuilder:resource:shortName=ld
+// +crossbuilder:generate:xrd:claimNames:kind=LogicalDatabaseClaim,plural=logicaldatabaseclaims
+// +crossbuilder:generate:xrd:defaultCompositionRef:name=logical-database
+// +crossbuilder:generate:xrd:enforcedCompositionRef:name=logical-database
+type RdsLogicalDatabase struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   RdsLogicalDatabaseSpec   `json:"spec"`
+	Status RdsLogicalDatabaseStatus `json:"status,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+type RdsLogicalDatabaseList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []RdsLogicalDatabase `json:"items"`
+}
+
+// RdsLogicalDatabaseSpec defines the desired state of RdsLogicalDatabase
+type RdsLogicalDatabaseSpec struct {
+	xpv1.ResourceSpec `json:",inline"`
+
+	// Sets the parameters for the provisioning
+	RdsLogicalDatabaseParameters `json:",inline"`
+
+	// KubernetesProviderConfig is the provider config for the Kubernetes provider.
+	//
+	// +required
+	KubernetesProviderConfig *xpv1.Reference `json:"kubernetesProviderConfig,omitempty"`
+}
+
+// Defines the parameters for the RDS provisioning
+type RdsLogicalDatabaseParameters struct {
+	// Databases is a map of databases to create.
+	//
+	// +required
+	Databases map[DatabaseName]SqlUsers `json:"databases,omitempty"`
+
+	// The type of database engine being provisioned
+	//
+	// +optional
+	// +default="postgres"
+	// +kubebuilder:validation:Enum=postgres;mysql;aurora-postgresql;aurora-mysql
+	Engine *string `json:"engine,omitempty"`
+}
+
+// RdsLogicalDatabaseStatus defines the observed state of RdsLogicalDatabase
+type RdsLogicalDatabaseStatus struct {
+	xpv1.ConditionedStatus `json:",inline"`
+
+	// Is the provisioning ready
+	//
+	// +optional
+	Ready bool `json:"ready,omitempty"`
+
+	// Connection secrets created for the databases
+	//
+	// +optional
+	ConnectionSecrets RdsLogicalDatabaseConnectionSecrets `json:"connectionSecrets,omitempty"`
+}
+
+type RdsLogicalDatabaseConnectionSecrets struct {
+	// The name of the secret created specifically for the app
+	//
+	// +optional
+	App string `json:"app,omitempty"`
+
+	// A map of secret names created for users
+	//
+	// +optional
+	Users map[string]string `json:"users,omitempty"`
+}
+
+// Repository type metadata.
+var (
+	RdsLogicalDatabaseKind      = "RdsLogicalDatabase"
+	RdsLogicalDatabaseGroupKind = schema.GroupKind{
+		Group: XRDGroup,
+		Kind:  RdsLogicalDatabaseKind,
+	}.String()
+	RdsLogicalDatabaseKindAPIVersion   = RdsLogicalDatabaseKind + "." + GroupVersion.String()
+	RdsLogicalDatabaseGroupVersionKind = GroupVersion.WithKind(RdsLogicalDatabaseKind)
+)
