003f44b5b876d9b67d9911703e7f009f48981f59
Laszlo Uveges <laszlo@giantswarm.io>
2024-12-05 10:20:28 +0100

    Generate xlaszlo test composition with example data structure

---
+++ crossplane.giantswarm.io/xlaszlo/v1alpha1/doc.go
@@ -0,0 +1,5 @@
+// v1alpha1 contains API types that extend the Crossplane API.
+// +kubebuilder:object:generate=true
+// +groupName=xlaszlo.crossplane.giantswarm.io
+// +versionName=v1alpha1
+package v1alpha1
---
+++ crossplane.giantswarm.io/xlaszlo/v1alpha1/example_types.go
@@ -0,0 +1,62 @@
+package v1alpha1
+
+import (
+	xpv1 "github.com/crossplane/crossplane-runtime/apis/common/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+)
+
+// Repository type metadata.
+var (
+	ExampleKind      = "Example"
+	ExampleGroupKind = schema.GroupKind{
+		Group: XRDGroup,
+		Kind:  ExampleKind,
+	}.String()
+	ExampleKindAPIVersion   = ExampleKind + "." + GroupVersion.String()
+	ExampleGroupVersionKind = GroupVersion.WithKind(ExampleKind)
+)
+
+// +kubebuilder:object:root=true
+// +kubebuilder:storageversion
+// +genclient
+// +genclient:nonNamespaced
+//
+// +kubebuilder:resource:scope=Cluster,categories=crossplane
+// +kubebuilder:subresource:status
+// +kubebuilder:resource:shortName=ex
+// +crossbuilder:generate:xrd:claimNames:kind=ExampleClaim,plural=ExampleClaims
+// +crossbuilder:generate:xrd:defaultCompositionRef:name=test
+// +crossbuilder:generate:xrd:enforcedCompositionRef:name=test
+type Example struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   ExampleSpec   `json:"spec"`
+	Status ExampleStatus `json:"status,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+type ExampleList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+
+	Items []Example `json:"items"`
+}
+
+// Use the `spec` struct to contain parameters you might not want to share
+// when nesting XRDs - these will usually be parameters that may be defined
+// in a parent.
+
+type ExampleSpec struct {
+	xpv1.ResourceSpec `json:",inline"`
+
+	ExampleParameters `json:",inline"`
+}
+
+type ExampleParameters struct {
+}
+
+type ExampleStatus struct {
+	xpv1.ConditionedStatus `json:",inline"`
+}
