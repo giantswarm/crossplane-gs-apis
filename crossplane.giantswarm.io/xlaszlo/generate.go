aec6bfacd568a11d6abd255d4fc6fc86facce611
Laszlo Uveges <laszlo@giantswarm.io>
2024-12-17 12:11:06 +0100

    Clean up xlaszlo experiment

--- crossplane.giantswarm.io/xlaszlo/generate.go
+++
@@ -1,3 +0,0 @@
-package apis
-
-//go:generate xrd-gen paths=./... xrd:allowDangerousTypes=true,crdVersions=v1 object:headerFile=../../hack/boilerplate.go.txt output:artifacts:config=../../apis/xlaszlo
--- crossplane.giantswarm.io/xlaszlo/v1alpha1/doc.go
+++
@@ -1,5 +0,0 @@
-// v1alpha1 contains API types that extend the Crossplane API.
-// +kubebuilder:object:generate=true
-// +groupName=xlaszlo.crossplane.giantswarm.io
-// +versionName=v1alpha1
-package v1alpha1
