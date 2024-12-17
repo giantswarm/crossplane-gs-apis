57aabe793ba707fad1ebba75fb9d82834ccb40e6
Laszlo Uveges <laszlo@giantswarm.io>
2024-12-17 12:04:34 +0100

    Move over KCL code and stuctures to xappdeployment

--- crossplane.giantswarm.io/xappdeployment/compositions/app-deployment/main.go
+++ crossplane.giantswarm.io/xappdeployment/compositions/app-deployment/main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"strings"
+
+	xkcl "github.com/crossplane-contrib/function-kcl/input/v1beta1"
 	"github.com/giantswarm/crossplane-gs-apis/crossplane.giantswarm.io/xappdeployment/v1alpha1"
 
 	xapiextv1 "github.com/crossplane/crossplane/apis/apiextensions/v1"
 	"github.com/mproffitt/crossbuilder/pkg/generate/composition/build"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 type builder struct{}
@@ -27,6 +31,45 @@ func (b *builder) Build(c build.CompositionSkeleton) {
 		})
 
 	build.SetBasePath(TemplateBasePath)
+
+	kclCommon, err := build.LoadTemplate("templates/common.k")
+	if err != nil {
+		panic(err)
+	}
+
+	kclConfigMapemplate, err := build.LoadTemplate("templates/provision-configmap.k")
+	if err != nil {
+		panic(err)
+	}
+
+	kclLogicalDatabaseTemplate, err := build.LoadTemplate("templates/provision-logical-database.k")
+	if err != nil {
+		panic(err)
+	}
+
+	kclHelmReleaseTemplate, err := build.LoadTemplate("templates/provision-helmrelease.k")
+	if err != nil {
+		panic(err)
+	}
+
+	kclFooter := "items = _items"
+
+	// Add pipeline steps here
+	c.NewPipelineStep("function-provision-configmap").
+		WithFunctionRef(xapiextv1.FunctionReference{
+			Name: "function-kcl",
+		}).
+		WithInput(build.ObjectKindReference{
+			Object: &xkcl.KCLInput{
+				TypeMeta: metav1.TypeMeta{
+					APIVersion: "krm.kcl.dev/v1alpha1",
+					Kind:       "KCLInput",
+				},
+				Spec: xkcl.RunSpec{
+					Source: strings.Join([]string{kclCommon, kclConfigMapemplate, kclLogicalDatabaseTemplate, kclHelmReleaseTemplate, kclFooter}, "\n\n"),
+				},
+			},
+		})
 
 	// Add pipeline steps here
 
--- crossplane.giantswarm.io/xappdeployment/v1alpha1/appdeployment_types.go
+++ crossplane.giantswarm.io/xappdeployment/v1alpha1/appdeployment_types.go
@@ -2,6 +2,9 @@ package v1alpha1
 
 import (
 	xpv1 "github.com/crossplane/crossplane-runtime/apis/common/v1"
+	v2beta1 "github.com/fluxcd/helm-controller/api/v2beta1"
+	"github.com/fluxcd/pkg/apis/meta"
+	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 )
@@ -54,6 +57,144 @@ type AppDeploymentSpec struct {
 }
 
 type AppDeploymentParameters struct {
+	AppDeploymentConfigMapParameters `json:",inline"`
+
+	// KubernetesProviderConfig is the provider config for the Kubernetes provider.
+	//
+	// +required
+	KubernetesProviderConfig *xpv1.Reference `json:"kubernetesProviderConfig,omitempty"`
+}
+
+type AppDeploymentConfigMapParameters struct {
+	// Name of the configmap.
+	//
+	// +required
+	Name string `json:"name,omitempty"`
+
+	// Namespace of the configmap
+	//
+	// +required
+	Namespace string `json:"namespace,omitempty"`
+
+	// Message to store in the configmap.
+	//
+	// +required
+	Message string `json:"message,omitempty"`
+
+	// Database parameters.
+	//
+	// +optional
+	Database DatabaseParameters `json:"database,omitempty"`
+
+	// Deployment parameters.
+	//
+	// +optional
+	DeploymentParameters `json:",inline"`
+}
+
+type DatabaseParameters struct {
+	// The type of database engine being provisioned.
+	//
+	// +optional
+	// +default="postgres"
+	// +kubebuilder:validation:Enum=postgres;mysql;aurora-postgresql;aurora-mysql
+	Engine *string `json:"engine,omitempty"`
+
+	DatabaseEsoParameters `json:"eso,omitempty"`
+
+	// Provider config reference for the database claim.
+	//
+	// +optional
+	ProviderConfigRef ProviderConfigRefParameters `json:"providerConfigRef,omitempty"`
+}
+
+type DatabaseEsoParameters struct {
+	// Name of the secret that contains SSA details.
+	//
+	// +optional
+	ClusterSsaField string `json:"clusterSsaField,omitempty"`
+
+	// Configuration for the tenant cluster.
+	//
+	// +optional
+	TenantCluster TenantClusterParameters `json:"tenantCluster,omitempty"`
+}
+
+// TenantCluster is the configuration for the tenant cluster.
+//
+// +kubebuilder:object:root=false
+// +kubebuilder:object:generate=true
+type TenantClusterParameters struct {
+	// API endpoint URL of the target cluster.
+	//
+	// +optional
+	ApiServerEndpoint string `json:"apiServerEndpoint,omitempty"`
+
+	// Name of the target cluster.
+	//
+	// +optional
+	ClusterName string `json:"clusterName,omitempty"`
+
+	// Whether or not to enable `external-secrets-operator` object
+	// deployments using `provider-kubernetes.
+	//
+	// +optional
+	// +default=false
+	Enabled bool `json:"enabled,omitempty"`
+}
+
+type ProviderConfigRefParameters struct {
+	// Name of the provider config for the database claim
+	//
+	// +optional
+	Name string `json:"name,omitempty"`
+}
+
+type DeploymentParameters struct {
+	IngressHost string `json:"ingressHost,omitempty"`
+
+	Interval string `json:"interval,omitempty"`
+
+	StorageNamespace string `json:"storageNamespace,omitempty"`
+
+	Suspend bool `json:"suspend,omitempty"`
+
+	TargetNamespace string `json:"targetNamespace,omitempty"`
+
+	Timeout string `json:"timeout,omitempty"`
+
+	ChartParameters `json:"chart,omitempty"`
+
+	ReleaseName string `json:"releaseName,omitempty"`
+
+	ServiceAccountName string `json:"serviceAccountName,omitempty"`
+
+	// DependsOn may contain a meta.NamespacedObjectReference slice with
+	// references to HelmRelease resources that must be ready before the HelmRelease can be reconciled.
+	//
+	// +optional
+	DependsOn []meta.NamespacedObjectReference `json:"dependsOn,omitempty"`
+
+	// KubeConfig for reconciling the HelmRelease on a remote cluster.
+	//
+	// +optional
+	KubeConfig *meta.KubeConfigReference `json:"kubeConfig,omitempty"`
+
+	// ValuesFrom holds references to resources containing Helm values for this HelmRelease,
+	// and information about how they should be merged.
+	//
+	// +optional
+	ValuesFrom *v2beta1.ValuesReference `json:"valuesFrom,omitempty"`
+
+	// Values holds the values for this Helm release.
+	//
+	// +optional
+	Values *apiextensionsv1.JSON `json:"values,omitempty"`
+}
+
+type ChartParameters struct {
+	Name    string `json:"name,omitempty"`
+	Version string `json:"version,omitempty"`
 }
 
 type AppDeploymentStatus struct {
