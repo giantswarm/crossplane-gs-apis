52b5f18511fdf794378dacea00a6fb25cde472b8
Łukasz Piątkowski <piontec@gmail.com>
2025-02-04 15:43:16 +0100

    add stub code

---
+++ crossplane.giantswarm.io/xgithubrepo/compositions/github-repo/main.go
@@ -0,0 +1,55 @@
+// Copyright 2025 The crossplane-gs-apis Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"github.com/giantswarm/crossplane-gs-apis/crossplane.giantswarm.io/xgithubrepo/v1alpha1"
+
+	xapiextv1 "github.com/crossplane/crossplane/apis/apiextensions/v1"
+	"github.com/mproffitt/crossbuilder/pkg/generate/composition/build"
+)
+
+type builder struct{}
+
+var Builder = builder{}
+var TemplateBasePath string
+
+func (b *builder) GetCompositeTypeRef() build.ObjectKindReference {
+	return build.ObjectKindReference{
+		GroupVersionKind: v1alpha1.GithubRepoGroupVersionKind,
+		Object:           &v1alpha1.GithubRepo{},
+	}
+}
+
+func (b *builder) Build(c build.CompositionSkeleton) {
+	c.WithName("github-repo").
+		WithMode(xapiextv1.CompositionModePipeline).
+		WithLabels(map[string]string{
+			// Add labels for uniquely identifying this composition
+		})
+
+	build.SetBasePath(TemplateBasePath)
+
+	// Add pipeline steps here
+
+	// Add the auto-ready function at the end
+	// This ensures the XR is marked ready when all
+	//   created MRs are ready
+	c.NewPipelineStep("function-auto-ready").
+		WithFunctionRef(xapiextv1.FunctionReference{
+			Name: "function-auto-ready",
+		})
+
+}
---
+++ crossplane.giantswarm.io/xgithubrepo/v1alpha1/groupversion.go
@@ -0,0 +1,41 @@
+// Copyright 2025 The crossplane-gs-apis Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package v1alpha1
+
+import (
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"sigs.k8s.io/controller-runtime/pkg/scheme"
+)
+
+// Package type metadata.
+const (
+	XRDGroup   = "xgithubrepo.crossplane.giantswarm.io"
+	XRDVersion = "v1alpha1"
+)
+
+var (
+	// GroupVersion is the API Group Version used to register the objects
+	GroupVersion = schema.GroupVersion{Group: XRDGroup, Version: XRDVersion}
+
+	// SchemeBuilder is used to add go types to the GroupVersionKind scheme
+	SchemeBuilder = &scheme.Builder{GroupVersion: GroupVersion}
+
+	// AddToScheme adds the types in this group-version to the given scheme.
+	AddToScheme = SchemeBuilder.AddToScheme
+)
+
+func init() {
+	SchemeBuilder.Register(&GithubRepo{}, &GithubRepoList{})
+}
